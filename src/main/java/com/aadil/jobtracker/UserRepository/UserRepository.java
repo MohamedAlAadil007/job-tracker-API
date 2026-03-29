7e9b59ffdca5c6e38c2866ff23563973ffb9a469
Aadil <itsmeaadil143@gmail.com>
2026-03-29 13:38:25 +0530

    Added UserRepository

--- src/main/java/com/aadil/jobtracker/UserRepository/UserRepository.java
+++ src/main/java/com/aadil/jobtracker/UserRepository/UserRepository.java
@@ -3,5 +3,5 @@ package com.aadil.jobtracker.UserRepository;
 import com.aadil.jobtracker.UserEntity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Long, UserEntity> {
+public interface UserRepository extends JpaRepository<UserEntity,Long> {
 }
