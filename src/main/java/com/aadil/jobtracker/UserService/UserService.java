35896bd40e5703f4b9d9676593705f255fdba8b3
Aadil <itsmeaadil143@gmail.com>
2026-04-12 13:35:17 +0530

    Tested the Paged Response Class. Worked Successfully

--- src/main/java/com/aadil/jobtracker/UserService/UserService.java
+++ src/main/java/com/aadil/jobtracker/UserService/UserService.java
@@ -4,8 +4,7 @@ import com.aadil.jobtracker.UserRepository.SortDirection;
 import com.aadil.jobtracker.UserRepository.UserSortField;
 import com.aadil.jobtracker.Validation.UserRequestDTO;
 import com.aadil.jobtracker.Validation.UserResponseDTO;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.data.domain.Page;
+import com.aadil.jobtracker.responses.PagedResponse;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,7 +17,7 @@ public interface UserService {
 
     Optional<UserResponseDTO> getUser(Long id);
 
-    Page<@NotNull UserResponseDTO> getAllUsers(String search, int page, int size, UserSortField sortField, SortDirection sortDirection);
+    PagedResponse<UserResponseDTO> getAllUsers(String search, int page, int size, UserSortField sortField, SortDirection sortDirection);
 
     List<UserResponseDTO> getUsersById(List<Long> ids);
 
